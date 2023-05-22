8c372ed4e822b6fc6b2a0db0e189a4607438525c
Luiz Gustavo Pfitscher e Feldmann <lfeldmann@ael.com.br>
2023-05-22 19:50:33 -0300

    Adding Ethernet library submodule

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ void setup()
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
